32a7d5b5c09b3960a64ba75725f88c6dce69b666
Gauvain Palanga <gauvainpalanga@gmail.com>
2026-06-03 08:53:10 +0200

    fix: lsp setup

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -104,6 +104,19 @@ require("mason-lspconfig").setup({
   ensure_installed = servers
 })
 
+for _, server in ipairs(servers) do
+  vim.lsp.enable(server)
+end
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local opts = { buffer = args.buf }
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gI", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+  end,
+})
+
 vim.diagnostic.config({
   virtual_text = true,
 })
